c2de8671fdf2abf02ebf66fdb0e62d4dacb7213a
Liliia423 <etude4408@gmail.com>
2025-01-24 18:54:04 +0200

    rty

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,27 +5,32 @@ import styles from './Profile.module.css';
 export default function Profile({ name, tag, location, image, stats }) {
   return (
     <div className={styles['profile-card']}>
-     
-      <img
-        src={image}
-        alt="User avatar"
-      />
+      <div className={styles['avatar-container']}>
+        <img
+          src={image}
+          alt="User avatar"
+          className={styles['avatar-image']}
+        />
+      </div>
       <p className={styles['user-name']}>{name}</p>
-      <p className={styles['user-about']}>@{tag}</p>
-        <p className={styles['user-about']}>{location}</p>
-      
-      <ul>
-        <li>
-          <span>Followers:</span>
-          <span>{stats.followers}</span>
+
+      <div className={styles['user-about']}>
+        <p >@{tag}</p>
+        <p >{location}</p>
+      </div>
+
+      <ul className={styles['users-statistic']}>
+        <li className={styles['users-react']}>
+          <span className={styles['users-react-title']}>Followers</span>
+          <span className={styles['users-react-statistic']}>{stats.followers}</span>
         </li>
-        <li>
-          <span>Views:</span>
-          <span>{stats.views}</span>
+        <li className={`${styles['users-react']} ${styles['views']}`}>
+          <span className={styles['users-react-title']}>Views</span>
+          <span className={styles['users-react-statistic']}>{stats.views}</span>
         </li>
-        <li>
-          <span>Likes:</span>
-          <span>{stats.likes}</span>
+        <li className={styles['users-react']}>
+          <span className={styles['users-react-title']}>Likes</span>
+          <span className={styles['users-react-statistic']}>{stats.likes}</span>
         </li>
       </ul>
     </div>
