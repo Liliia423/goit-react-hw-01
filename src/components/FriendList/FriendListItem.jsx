7d2b5b821e394eb2cbff9a58157bea584c021209
Liliia423 <etude4408@gmail.com>
2025-01-25 10:47:57 +0200

    rty

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -4,14 +4,16 @@ export default function FriendListItem({ avatar, name, isOnline }) {
   return (
     <li className={styles['friend-item']}>
       <img className={styles['avatar']} src={avatar} alt="Avatar" width="48" />
-      <p className={styles['name']}>{name}</p>
-      <p
-        className={`${styles['status']} ${
-          isOnline ? styles['online'] : styles['offline']
-        }`}
-      >
-        {isOnline ? 'Online' : 'Offline'}
-      </p>
+      <div className={styles['name-and-atatus']}>
+        <p className={styles['name']}>{name}</p>
+        <p
+          className={`${styles['status']} ${
+            isOnline ? styles['online'] : styles['offline']
+          }`}
+        >
+          {isOnline ? 'Online' : 'Offline'}
+          </p>
+      </div>
     </li>
   );
 }
